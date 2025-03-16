6e207378b45340a9602b950d97b5b2b39c6290a0
ashish deshpande <reachashish2004@gmail.com>
2025-03-16 12:33:09 +0530

    Removed lib folder from Git tracking

--- test/unit/ZkSyncDevOps.t.sol
+++ test/unit/ZkSyncDevOps.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.18;
 
 import {Test, console} from "../../lib/forge-std/src/Test.sol";
 import {ZkSyncChainChecker} from "../../lib/foundry-devops/src/ZkSyncChainChecker.sol";
